c45510487052bb15402eae1036c796f73d1b4af5
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-03-11 02:44:26 +0000

    code refactor and reset work

---
+++ NOT USING/gridsetup.js
@@ -0,0 +1,58 @@
+function drawBoard() {
+    let gameTitle = document.createElement("h1");
+    gameTitle.innerText = "TIC TAC TOE";
+    document.body.appendChild(gameTitle);
+
+    let currentPlayerText = document.createElement("p");
+    currentPlayerText.innerText = "Current Player: FILL IN";
+    document.body.appendChild(currentPlayerText);
+
+    
+    
+    
+    // console.log(newTitle)
+    // newTitle.textContent = "Tic Tac Toe"
+    // const content = document.querySelector(".game")
+    // content.appendChild(newTitle)
+
+
+    //create board
+    for (let i = 1; i <= 9; i++){
+     let newDiv = document.createElement("div");
+      newDiv.classList.add("cell");
+     let grid = document.getElementById("game");
+      grid.appendChild(newDiv);
+   
+     newDiv.addEventListener('click', event => {
+       const target = event.target;
+        let playerLabel = target;
+        playerLabel.textContent = player;
+       //  target.appendChild(playerLabel);
+       playerChange();
+       }, { once: true });
+    }
+   }
+
+drawBoard();
+
+
+
+// function drawBoard()
+// {
+//     let parent = document.getElementById("grid-cell");
+//     let counter = 12;
+
+//     for (let i = 0; i < 3; i++)
+//     {
+//         let row = document.createElement("tr");
+
+//         for(let x = 0; x < 3; x++)
+//         {
+//             let col = document.createElement("td");
+//             col.innerHTML = counter;
+
+//             row.appendChild(col);
+//         }
+//         parent.appendChild(row);
+//     }
+// }
--- main.js
+++ main.js
@@ -20,7 +20,6 @@ class TicTacToe {
         this.currentPlayer = 'X';
         this.gameBoard = ["","","","","","","","",""]
         this.endGame = false;
-        this.turnCounter = 0;
         console.log(this)
     }
 
@@ -60,14 +59,6 @@ class TicTacToe {
     getEndGame() {
         return this.endGame;
     }
-
-    setTurnCounter(x) {
-        this.turnCounter = x;
-    }
-
-    getTurnCounter() {
-        return this.turnCounter;
-    }
 }
 
 
@@ -82,36 +73,32 @@ class GameView {
     console.log(this.cells);
   }
 
-  resetBoard = () => {
-    this.cells.forEach ((cell, i) => {
-      cell.innerText = ''
-    })
+  resetBoard = (handleClick) => {
+    this.cells.forEach ((cell, i) => {cell.innerText = ''})
     console.log("reset render")
-    this.render()
+    this.render(handleClick)
+  }
+
+  init(resetGame) {
+    let gameTitle = document.createElement("h1");
+    gameTitle.classList = "game-title"
+    gameTitle.innerText = "TIC TAC TOE";
+    document.body.appendChild(gameTitle);
+
+    this.cells = Array.from(this.cells);
+    
+    this.restartButton.addEventListener("click", resetGame);
   }
   
-  render(model, resetGame) {
+  render(handleClick) {
     // @TODO: create element div for the grid here
+    // title
 
-    this.restartButton.addEventListener("click", resetGame);
 
     // Block of code to place and modify the grid cells with X's and O's
-    this.cells = Array.from(this.cells);
     this.cells.forEach((cell, i) => {
-      cell.addEventListener("click", () => {
-          // appends innertext to player's symbol
-          cell.innerText = model.currentPlayer;
-          console.log('this is cell', i)
-          model.setGameBoard(i);
-          model.checkWin(); // CHECKING THISSSSSS
-          model.switchCurrentPlayer();
-          document.getElementById("current-player").innerHTML = `Current Player: ${model.currentPlayer}`;
-        },
-        {
-          // only allows cell to be clicked once and turns off eventlistener
-          once: true,
-        }
-      );
+      // only allows cell to be clicked once and turns off eventlistener
+      cell.addEventListener("click", (e) => handleClick(e, i), { once: true });
     }); 
   }
 }
@@ -123,24 +110,33 @@ class GameController {
         this.model = model;
         this.view = view;
         console.log(this.view)
-        this.view.render(this.model, this.resetGame);
-
+        this.view.init(this.resetGame);
+        this.view.render(this.handleClick);
     }
+
+    // need to remove the event listener on cells (this.something.cells removeeventlistener)
 
     resetGame = () => {
-      this.model.currentPlayer = 'X';
-      this.model.gameBoard = ["","","","","","","","",""];
-      this.model.endGame = false;
-      this.model.turnCounter = 0;
-      this.view.resetBoard()
+      this.model.currentPlayer = 'X'; // these should use setters, this.model.setPlayer('') 
+      this.model.gameBoard = ["","","","","","","","",""]; // these should use setters
+      this.model.endGame = false; // these should use setters
+      this.view.resetBoard(this.handleClick);
     }
 
-    // displayPlayer(
-    //   this.model.currentPlayer
-    // )
+    handleClick = (e, i) => {
+      // appends innertext to player's symbol
+      e.target.innerText = this.model.currentPlayer;
+      console.log('this is cell', i)
+      this.model.setGameBoard(i);
+      this.model.checkWin();
+      this.model.switchCurrentPlayer();
+      //document.getElementsByClassName("game-title").innerText = `Current Player: ${model.currentPlayer}`;
+      document.getElementById("current-player").innerHTML = `Current Player: ${this.model.currentPlayer}`;
+    }
 
     // TODO
-    // - Create restart game function
+    // - Finish restart game function
+    //    - Create init(), 
     // - Create winning message function
     // - Create draw message function
     
