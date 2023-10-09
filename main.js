6b786b6abbb642127b59bdf3873f726150c2db04
Nick McMillen <nickolas.mcmillen@gmail.com>
2023-10-09 13:17:12 -0400

    Updated all main.js logic

--- main.js
+++ main.js
@@ -1,152 +1,139 @@
-console.log('hello tictactoe')
-
 const WINNING_CONDITIONS = [
-    // Horizontal
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-    // Vertical
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-    // Diagonal
-      [0, 4, 8],
-      [2, 4, 6]
-  ];
+            // Horizontal
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            // Vertical
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            // Diagonal
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
 
-// **MODEL**
-class TicTacToe {
-    constructor() {
-        this.currentPlayer = 'X';
-        this.gameBoard = ["","","","","","","","",""]
-        this.endGame = false;
-        console.log(this)
-    }
+        // **MODEL**
+        class TicTacToe {
+            constructor() {
+                this.currentPlayer = 'X';
+                this.gameBoard = ["", "", "", "", "", "", "", "", ""];
+                this.endGame = false;
+                console.log(this);
+            }
 
-    checkWin() {
-      WINNING_CONDITIONS.some((inline) => {
-        if(this.gameBoard[inline[0]] &&
-          this.gameBoard[inline[0]] === this.gameBoard[inline[1]] &&
-          this.gameBoard[inline[1]] === this.gameBoard[inline[2]]) {
-            // console.log(this.gameBoard[inline[0]])
-            console.log(`${this.currentPlayer} wins!`)
-            alert(`${this.currentPlayer} wins!`)
-              return true
-              // need to add something to fire off and change endGame to true and populate lock the board and win message
-          }
-      })
-    }
+            checkWin() {
+                return WINNING_CONDITIONS.some((inline) => {
+                    if (
+                        this.gameBoard[inline[0]] &&
+                        this.gameBoard[inline[0]] === this.gameBoard[inline[1]] &&
+                        this.gameBoard[inline[1]] === this.gameBoard[inline[2]]
+                    ) {
+                        this.endGame = true;
+                        console.log(`${this.currentPlayer} wins!`);
+                        document.getElementById("win-message").innerText = `${this.currentPlayer} wins!`;
+                        document.getElementById("win-message").style.display = "block";
+                        return true;
+                    }
+                });
+            }
 
-    switchCurrentPlayer() {
-        this.currentPlayer = this.currentPlayer == "X" ? "O" : "X";
-    }
+            switchCurrentPlayer() {
+                this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+            }
 
-    getCurrentPlayer() {
-        return this.currentPlayer;
-    }
+            getCurrentPlayer() {
+                return this.currentPlayer;
+            }
 
-    setGameBoard(i) {
-        this.gameBoard[i] = this.currentPlayer;
-    }
+            setGameBoard(i) {
+                this.gameBoard[i] = this.currentPlayer;
+            }
 
-    getGameBoard() {
-        return this.gameBoard;
-    }
+            getGameBoard() {
+                return this.gameBoard;
+            }
 
-    setEndGame(x) {
-        this.endGame= x;
-    }
+            setEndGame(x) {
+                this.endGame = x;
+            }
 
-    getEndGame() {
-        return this.endGame;
-    }
-}
+            getEndGame() {
+                return this.endGame;
+            }
+        }
 
+        // **VIEW**
+        class GameView {
+            constructor() {
+                this.cells = document.querySelectorAll(".grid-cell");
+                this.winMessage = document.getElementById("win-message");
+                this.drawMessage = document.getElementById("draw-message");
+                this.restartButton = document.getElementById("restart-button");
+                console.log(this.cells);
+            }
 
-// **VIEW**
-class GameView {
-  constructor() {
-    this.cells = document.querySelectorAll(".grid-cell");
-    this.winMessage = document.getElementById("win-message");
-    this.drawMessage = document.getElementById("draw-message");
-    this.restartButton = document.getElementById("restart-button");
-    // this.currentPlayer = document.getElementById("current-player")
-    console.log(this.cells);
-  }
+            resetBoard(handleClick) {
+                this.cells.forEach((cell, i) => {
+                    cell.innerText = '';
+                    cell.removeEventListener("click", handleClick);
+                });
+                this.winMessage.style.display = "none";
+                this.drawMessage.style.display = "none";
+                console.log("reset render");
+                this.render(handleClick);
+            }
 
-  resetBoard = (handleClick) => {
-    this.cells.forEach ((cell, i) => {
-      cell.innerText = '';
-      // console.dir(cell)
-      // removeEventListener("click", handleClick)
-      // console.dir("after", cell)
-    })
-    console.log("reset render")
-    this.render(handleClick)
-  }
+            init(resetGame) {
+                this.cells = Array.from(this.cells);
 
-  init(resetGame) {
-    // TITLE
-    let gameTitle = document.createElement("h1");
-    gameTitle.classList = "game-title";
-    gameTitle.innerText = "TIC TAC TOE";
-    document.body.appendChild(gameTitle);
+                this.restartButton.addEventListener("click", resetGame);
+            }
 
-    this.cells = Array.from(this.cells);
-    
-    this.restartButton.addEventListener("click", resetGame);
-  }
-  
-  render(handleClick) {
-    // Block of code to place and modify the grid cells with X's and O's
-    this.cells.forEach((cell, i) => {
-      cell.addEventListener("click", (e) => handleClick(e, i), { once: true });
-    }); 
-  }
-}
+            render(handleClick) {
+                this.cells.forEach((cell, i) => {
+                    cell.addEventListener("click", (e) => handleClick(e, i), { once: true });
+                });
+            }
+        }
 
+        // **CONTROLLER
+        class GameController {
+            constructor(model, view) {
+                this.model = model;
+                this.view = view;
+                console.log(this.view);
+                this.view.init(this.resetGame);
+                this.view.render(this.handleClick);
+            }
 
-// **CONTROLLER
-class GameController {
-    constructor(model, view) {
-        this.model = model;
-        this.view = view;
-        console.log(this.view)
-        this.view.init(this.resetGame);
-        this.view.render(this.handleClick);
-    }
+            resetGame = () => {
+                this.model.currentPlayer = 'X';
+                this.model.gameBoard = ["", "", "", "", "", "", "", "", ""];
+                this.model.endGame = false;
+                this.view.resetBoard(this.handleClick);
+                console.log(this.view.cells);
+            };
 
-    // need to remove the event listener on cells (this.something.cells removeeventlistener)
+            handleClick = (e, i) => {
+                if (this.model.getEndGame() || this.model.getGameBoard()[i] !== "") {
+                    return; // Prevent further moves when the game is over or the cell is already filled.
+                }
 
-    resetGame = () => {
-      // this.model.currentPlayer = 'X'; // these should use setters, this.model.setPlayer('')
-      this.model.switchCurrentPlayer();
-      this.model.gameBoard = ["","","","","","","","",""]; // these should use setters
-      this.model.endGame = false; // these should use setters
-      this.view.resetBoard(this.handleClick);
+                e.target.innerText = this.model.currentPlayer;
+                this.model.setGameBoard(i);
 
-      // this.view.cells.removeEventListener("click", something here)
+                if (this.model.checkWin()) {
+                    // Check for a win
+                    this.model.setEndGame(true);
+                } else if (this.model.getGameBoard().every((cell) => cell !== "")) {
+                    // Check for a draw and show draw message
+                    document.getElementById("draw-message").style.display = "block";
+                    this.model.setEndGame(true);
+                } else {
+                    this.model.switchCurrentPlayer();
+                    document.getElementById("current-player").innerText = `Current Player: ${this.model.getCurrentPlayer()}`;
+                }
+            };
+        }
 
-      console.log(this.view.cells)
-    }
-
-    handleClick = (e, i) => {
-      // appends innertext to player's symbol
-      e.target.innerText = this.model.currentPlayer;
-      console.log('this is cell', i)
-      this.model.setGameBoard(i);
-      this.model.checkWin();
-      this.model.switchCurrentPlayer();
-      // document.getElementsByClassName("player-status").innerHTML = `Current Player: ${model.currentPlayer}`;
-      document.getElementById("current-player").innerText = `Current Player: ${this.model.currentPlayer}`;
-    }
-
-    // TODO
-    // - Finish restart game function
-    //    - Create init(), 
-    // - Create winning message function
-    // - Create draw message function
-    
-  }
-  
-  new GameController(new TicTacToe(), new GameView());
+        new GameController(new TicTacToe(), new GameView());
